372992a790395d29152b1cdcb65a26feb3bbc654
EdwardNee <nieduard_01@mail.ru>
2021-08-26 21:54:49 +0300

    xml code of current day info

--- app/src/main/java/com/nieduard/weather_avito/MainActivity.kt
+++ app/src/main/java/com/nieduard/weather_avito/MainActivity.kt
@@ -2,10 +2,24 @@ package com.nieduard.weather_avito
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null)
+            moveToFragment(WeatherFragment())
+    }
+
+    /**
+     * Moving to given fragment.
+     */
+    private fun moveToFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.wrapper_main, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
